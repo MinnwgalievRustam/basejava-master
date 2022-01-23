82bc22ad88b9f1d092aebe023ef04408457851f8
Rustam <rustamminnegaliev1997@gmail.com>
2022-01-24 03:08:33 +0500

    removed the method getAll, added method getAllSorted

--- src/com/example/webapp/storage/Storage.java
+++ src/com/example/webapp/storage/Storage.java
@@ -2,12 +2,20 @@ package com.example.webapp.storage;
 
 import com.example.webapp.model.Resume;
 
+import java.util.List;
+
 public interface Storage {
     void clear();
+
     void update(Resume r);
+
     void save(Resume r);
+
     Resume get(String uuid);
+
     void delete(String uuid);
-    Resume[] getAll();
+
+    List<Resume> getAllSorted();
+
     int size();
 }
