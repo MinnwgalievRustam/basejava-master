09881c567a9209c4b69e618a327ffcb5016b32ee
Rustam <rustamminnegaliev1997@gmail.com>
2022-01-31 10:13:58 +0500

    Created Junit test MapResumeStorageTest

---
+++ test/com/example/webapp/storage/MapResumeStorageTest.java
@@ -0,0 +1,9 @@
+package com.example.webapp.storage;
+
+import static org.junit.Assert.*;
+
+public class MapResumeStorageTest extends AbstractStorageTest {
+    public MapResumeStorageTest() {
+        super(new MapResumeStorage());
+    }
+}
